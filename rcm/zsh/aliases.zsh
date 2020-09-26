fd5c60174a8fd3909012b7b5e198bfde2a562ecc
Niklas Wigertz <niklas.wigertz@briteback.com>
2020-09-26 22:40:12 +0200

    feat: add starship and aws-vault

--- rcm/zsh/aliases.zsh
+++ rcm/zsh/aliases.zsh
@@ -5,6 +5,12 @@ alias ohmyzsh="code ~/.oh-my-zsh"
 # Brew
 alias brewup="brew update; brew upgrade; brew cleanup; brew doctor"
 
+# AWS
+alias ava="aws-vault add"
+alias ave="aws-vault exec"
+alias avl="aws-vault list"
+alias asp="echo \"use aws-vault! ava, ave, avl\""
+
 # Terragrunt
 alias ti="terragrunt init"
 alias tp="terragrunt plan"
--- rcm/zsh/general.zsh
+++ rcm/zsh/general.zsh
@@ -1,2 +1,5 @@
 # k8s
 [[ /usr/local/bin/kubectl ]] && source <(kubectl completion zsh)
+
+# AWS
+export AWS_SDK_LOAD_CONFIG=1
