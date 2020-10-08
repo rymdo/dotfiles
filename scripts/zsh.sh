a51a0c07092162534cc82f9b31efff67d4028af2
Niklas Wigertz <niklas.wigertz@briteback.com>
2020-10-08 15:24:37 +0200

    fix: change zsh path

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -1,5 +1,5 @@
 install_zsh() {
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
     echo "Change default shell for OSX. Requires password...."
-    sudo dscl . -create /Users/$USER UserShell /usr/local/bin/zsh
+    sudo dscl . -create /Users/$USER UserShell /bin/zsh
 }
