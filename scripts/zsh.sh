8910b186e6663704f7fa57364a4ceab05b37d065
Niklas Wigertz <niklas.wigertz@briteback.com>
2020-10-10 12:57:21 +0200

    change: add check for changing default shell

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -1,5 +1,13 @@
 install_zsh() {
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-    echo "Change default shell for OSX. Requires password...."
-    sudo dscl . -create /Users/$USER UserShell /bin/zsh
+    _set_default_shell
+}
+
+_set_default_shell() {
+    local currentShell=$(dscl /Search read /Users/$USER UserShell | cut -d ' ' -f 2)
+    echo "current shell: ${currentShell}"
+    if [ ! "$currentShell" = "/bin/zsh" ]; then
+        echo "Changing default shell for OSX. Requires password...."
+        sudo dscl . -create /Users/$USER UserShell /bin/zsh
+    fi
 }
