c3c44a01c700bc15a95fc1e7859a2ffed9796c4f
Niklas Wigertz <niklas.wigertz@briteback.com>
2021-02-25 09:45:49 +0100

    feat: add spaceship kubernetes config

--- rcm/zsh/prompt.zsh
+++ rcm/zsh/prompt.zsh
@@ -1,4 +1,13 @@
 export CLICOLOR=1
 export LSCOLORS=GxFxCxDxBxegedabagaced  
 autoload -U promptinit; promptinit
+
+sed "
+s|<AWS_EKS_CLUSTER_ARN>|$AWS_EKS_CLUSTER_ARN|g
+s|<AWS_EKS_CLUSTER_LIVE>|$AWS_EKS_CLUSTER_LIVE|g
+s|<AWS_EKS_CLUSTER_STAGING>|$AWS_EKS_CLUSTER_STAGING|g
+s|<AWS_EKS_CLUSTER_TEST>|$AWS_EKS_CLUSTER_TEST|g
+s|<AWS_EKS_CLUSTER_DEV>|$AWS_EKS_CLUSTER_DEV|g
+" ~/.config/starship.template.toml > ~/.config/starship.toml
+
 eval "$(starship init zsh)"
